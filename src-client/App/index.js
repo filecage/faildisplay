1626b0b2c4d8bcd7479181136768a2e5c7110c0c
David Beuchert <mail@dbeuchert.com>
2016-06-14 18:35:40 +0200

    app keeps services in sync

--- src-client/App/index.js
+++ src-client/App/index.js
@@ -9,6 +9,41 @@ export default class App extends EventEmitter {
         this._services = [];
     }
     
+    sync() {
+        this._socket.on('message', (type, message) => {
+            switch (type) {
+                case 'initialState':
+                    this._services = message.payload.map(service => Service.createFromObject(service));
+                    this.emit('update');
+                    break;
+
+                case 'update':
+                    var newService = message.payload.newService;
+                    this._services = this._services.map(service => {
+                        if (service.getId() !== newService.id) {
+                            return service;
+                        }
+
+                        return Service.createFromObject(newService);
+                    });
+                    this.emit('update');
+                    break;
+
+                case 'insert':
+                    this._services.push(Service.createFromObject(message.payload));
+                    this.emit('update');
+                    break;
+
+                case 'delete':
+                    this._services.filter(service => {
+                        return service.getId() !== message.payload.id;
+                    });
+                    this.emit('update');
+                    break;
+            }
+        })
+    }
+
     getServices () {
         return this._services;
     }
