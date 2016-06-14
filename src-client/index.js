5bb0fdd6f37d874c00f27be2999f09f9dfe61e42
David Beuchert <mail@dbeuchert.com>
2016-06-14 18:36:03 +0200

    initialize app, render display to app container

--- src-client/index.js
+++ src-client/index.js
@@ -1,1 +1,16 @@
-// hello world
+import React from 'react';
+import ReactDOM from 'react-dom';
+import Display from './Components/Containers/Display';
+import App from './App';
+import Socket from './Socket';
+
+var socket = new Socket('ws://localhost:3000');
+var app = new App(socket);
+app.sync();
+
+ReactDOM.render(
+    <Display app={app}/>,
+    document.getElementById('app')
+);
+
+socket.connect();
