4c3dcc07c6c07b6ac19dc99a238773c97746a4db
David Beuchert <mail@dbeuchert.com>
2016-06-14 19:19:11 +0200

    use let instead of var

--- src-client/index.js
+++ src-client/index.js
@@ -4,9 +4,9 @@ import Display from './Components/Containers/Display';
 import App from './App';
 import Socket from './Socket';
 
-var socket = new Socket('ws://localhost:3000');
-var app = new App(socket);
 app.sync();
+let socket = new Socket('ws://localhost:3000');
+let app = new App(socket);
 
 ReactDOM.render(
     <Display app={app}/>,
