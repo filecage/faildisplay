ea9b68505779628a719292a9cc54201a89ae17e5
David Beuchert <mail@dbeuchert.com>
2016-06-14 17:18:20 +0200

    app holds services

--- src-client/App/index.js
+++ src-client/App/index.js
@@ -1,9 +1,15 @@
 import {EventEmitter} from 'events';
+import Service from '../Models/Service';
 
 export default class App extends EventEmitter {
     constructor(socket) {
         super();
-        
+
         this._socket = socket;
+        this._services = [];
+    }
+    
+    getServices () {
+        return this._services;
     }
 }
