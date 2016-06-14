8de25b1436077a9caa43790fd4fc76b1619e15dd
David Beuchert <mail@dbeuchert.com>
2016-06-14 18:35:31 +0200

    set state from app and render from state

--- src-client/Components/Containers/Display.js
+++ src-client/Components/Containers/Display.js
@@ -2,10 +2,22 @@ import React from 'react';
 import Service from '../Service';
 
 export default class Display extends React.Component {
+    constructor(props) {
+        super(props);
+        this.state = Object.assign({}, props);
+        this.state.services = this.props.app.getServices();
+
+        this.props.app.on('update', () => {
+            this.setState({
+                services: this.props.app.getServices()
+            });
+        })
+    }
+
     render() {
         return (
             <ul>
-                {this.props.app.getServices().map(service => <Service service={service} key={service.getId()}/>)}
+                {this.state.services.map(service => <Service service={service} key={service.getId()}/>)}
             </ul>
         );
     }
