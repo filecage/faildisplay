c2b5246e608d07896034c2edac41944786f4409f
David Beuchert <mail@dbeuchert.com>
2016-06-15 10:08:26 +0200

    use props instead of state, create component via connect()

--- src-client/Components/Containers/Display.js
+++ src-client/Components/Containers/Display.js
@@ -1,24 +1,21 @@
+import {connect} from 'react-redux';
 import React from 'react';
 import Service from '../Service';
 
-export default class Display extends React.Component {
-    constructor(props) {
-        super(props);
-        this.state = Object.assign({}, props);
-        this.state.services = this.props.app.getServices();
-
-        this.props.app.on('update', () => {
-            this.setState({
-                services: this.props.app.getServices()
-            });
-        })
-    }
-
+export class Display extends React.Component {
     render() {
         return (
             <ul>
-                {this.state.services.map(service => <Service service={service} key={service.getId()}/>)}
+                {this.props.services.map(service => <Service service={service} key={service.getId()}/>)}
             </ul>
         );
     }
 }
+
+function mapStateToProps(state) {
+    return {
+        services: state.services
+    }
+}
+
+export default connect(mapStateToProps)(Display);
