1b6be75c6dcb5fa682f62fb8c2e7ed5e41dd2811
David Beuchert <mail@dbeuchert.com>
2016-06-15 13:37:30 +0200

    draw sub service map

--- src-client/Components/Service/index.js
+++ src-client/Components/Service/index.js
@@ -2,6 +2,14 @@ import React from 'react';
 
 export default class Service extends React.Component {
     render () {
-        return (<div>{this.props.service.getName()} is {this.props.service.getStatus()}</div>);
+        var subServices = this.props.service.getSubServices();
+
+        return (<li>{this.props.service.getName()} is {this.props.service.getStatus()}{subServices.length ? this.buildSubServiceList(subServices) : null}</li>);
+    }
+
+    buildSubServiceList (subServices) {
+        return (<ul>
+            {subServices.map(subService => <Service service={subService} key={subService.getId()}/>)}
+        </ul>);
     }
 }
