d807b3c71e77562e9b021e0fb874185ec9016586
David Beuchert <mail@dbeuchert.com>
2016-06-15 10:07:33 +0200

    use provider and store from redux

--- src-client/index.js
+++ src-client/index.js
@@ -3,13 +3,19 @@ import ReactDOM from 'react-dom';
 import Display from './Components/Containers/Display';
 import App from './App';
 import Socket from './Socket';
+import Reducers from './Reducers';
+import {Provider} from 'react-redux';
+import {createStore} from 'redux';
 
 app.sync();
+let store = createStore(Reducers);
 let socket = new Socket('ws://localhost:3000');
 let app = new App(socket);
 
 ReactDOM.render(
-    <Display app={app}/>,
+    <Provider store={store}>
+        <Display/>
+    </Provider>,
     document.getElementById('app')
 );
 
