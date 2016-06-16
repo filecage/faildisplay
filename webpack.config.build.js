985da5e2db105f5379059affa45e7dc7d57b5eba
David Beuchert <mail@dbeuchert.com>
2016-06-16 16:25:59 +0200

    adds dedupe und uglifyjs plugin

--- webpack.config.build.js
+++ webpack.config.build.js
@@ -1,7 +1,10 @@
+var webpack = require('webpack');
 var ExtractTextPlugin = require('extract-text-webpack-plugin');
 var config = require('./webpack.config.base');
 
 config.plugins.push(new ExtractTextPlugin('style.css'));
+config.plugins.push(new webpack.optimize.UglifyJsPlugin());
+config.plugins.push(new webpack.optimize.DedupePlugin());
 config.module.loaders.push({
     test: /\.less$/,
     loader: ExtractTextPlugin.extract("css!less")
